2f68ad1faab0ec05e34bcb92171fa284342e41b5
Jenise Ruehle <jela07@gmail.com>
2020-03-20 22:24:20 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+class Artist
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+   @name = name
+   self.class.all << self
+   @songs = []
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def songs
+    @songs
+  end 
+  
+  def add_song(song)
+    @songs << song
+  end 
+  
+  def self.find_or_create_by_name(name)
+    
+  end 
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song 
+  attr_accessor :name, :artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    self.class.all << self
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.new_by_filename(filename)
+    song_name = filename.split(" - ")[1]
+    artist = filename.split(" - ")[0]
+    song = self.new(song_name)
+    song.artist= artist
+    song
+  end 
+  
+end 
