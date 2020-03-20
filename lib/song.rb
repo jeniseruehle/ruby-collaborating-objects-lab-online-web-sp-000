87c3833782e73941c8d8b76ede496a31796739f8
Jenise Ruehle <jela07@gmail.com>
2020-03-20 22:39:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
     song_name = filename.split(" - ")[1]
     song = self.new(song_name)
     song.artist = Artist.find_or_create_by_name(artist_name)
-    artist.songs << song
+    artist_name.songs << song
     song
   end 
   
