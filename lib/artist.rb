00b593007e59dc05823852f840549e4f237934f9
Jenise Ruehle <jela07@gmail.com>
2020-03-20 22:36:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,8 +21,11 @@ class Artist
     @songs << song
   end 
   
-  def self.find_or_create_by_name(artist_name)
-    
+  def self.find_or_create_by_name(name)
+    artist = @@all.find{|artist| artist.name == name}
+    if artist == nil 
+      artist = self.new(name)
+      artist
     
   end 
   
